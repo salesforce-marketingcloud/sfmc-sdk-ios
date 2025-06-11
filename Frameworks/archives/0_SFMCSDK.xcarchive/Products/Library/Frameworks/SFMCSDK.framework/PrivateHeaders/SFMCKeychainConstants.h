54dd60fe0986739f89af49641693347a82f50123
Himadri Sekhar Jyoti <hjyoti@salesforce.com>
2025-06-11 14:46:51 +0530

    v1.1.4

---
+++ Frameworks/archives/0_SFMCSDK.xcarchive/Products/Library/Frameworks/SFMCSDK.framework/PrivateHeaders/SFMCKeychainConstants.h
@@ -0,0 +1,22 @@
+//
+//  SFMCKeychainConstants.h
+//  SFMCSDK
+//
+//  Copyright © 2023 Salesforce Marketing Cloud. All rights reserved.
+//
+
+#ifndef SFMCKeychainConstants_h
+#define SFMCKeychainConstants_h
+
+static NSString * const kGeneratedKeyStoreKeychainIdentifier = @"com.salesforce.keystore.generatedKeystoreKeychainId";
+static NSString * const kGeneratedKeyStoreEncryptionKeyKeychainIdentifier = @"com.salesforce.keystore.generatedKeystoreEncryptionKeyId";
+static NSString * const kBaseAppIdKeychainIdentifier = @"com.salesforce.security.baseappid";
+static NSString * const kBaseAppIdKeychainIdentifierSimulator = @"com.salesforce.security.baseappid.sim";
+static NSString * const kSFMCSDKBundleIdAppVersionKeychainIdentifier = @"com.salesforce.marketingcloud.SFMCSdk_appVersion";
+static NSString * const kSFMCSDKMigrateKeychainAccessibilityFlag = @"com.salesforce.marketingcloud.SFMCSdk.isAccessibilityMigrated";
+
+#endif /* SFMCKeychainConstants_h */
+
+
+
+
